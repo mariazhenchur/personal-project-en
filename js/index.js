c18c2781bff6d852f0901fc7eb6fe09f32542654
mariazhenchur <145922337+mariazhenchur@users.noreply.github.com>
2023-11-06 16:44:30 +0000

    h

--- js/index.js
+++ js/index.js
@@ -1,6 +1,4 @@
 document.addEventListener("partialsLoaded", async () => {
   await import("./burger-menu.js");
   await import('./blog.js');
-  await import('./section-carousel-price.js');
-  await import('./queen.js');
 });
